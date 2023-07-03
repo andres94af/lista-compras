a5a23dd30db92d9d7f69b23e29c1145e99b1749c
Andres Fernandez <andres94.af@gmail.con>
2023-07-03 13:23:15 +0200

    iniciando proyecto

---
+++ src/main/java/com/compras/ListaComprasBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.compras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaComprasBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaComprasBackEndApplication.class, args);
+	}
+
+}
